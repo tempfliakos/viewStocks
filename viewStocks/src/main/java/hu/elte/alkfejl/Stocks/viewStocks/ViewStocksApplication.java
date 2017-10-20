f0da660fd98e192c2b2b704041d7daeba937cc4f
Tempfli Ákos <tempfliakos@icloud.com>
2017-10-20 14:37:48 +0200

    empty project

---
+++ viewStocks/src/main/java/hu/elte/alkfejl/Stocks/viewStocks/ViewStocksApplication.java
@@ -0,0 +1,12 @@
+package hu.elte.alkfejl.Stocks.viewStocks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViewStocksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViewStocksApplication.class, args);
+	}
+}

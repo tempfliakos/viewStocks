c3073625f42414c1e86f31f86fc4bbb2dc580c3f
Bence Knáb <bknab@loginet.hu>
2017-11-15 15:33:36 +0100

    akos semmi test

--- viewStocks/src/main/java/hu/elte/alkfejl/stocks/viewstocks/util/Response.java
+++ viewStocks/src/main/java/hu/elte/alkfejl/stocks/viewstocks/util/Response.java
@@ -2,18 +2,20 @@ package hu.elte.alkfejl.Stocks.viewStocks.util;
 
 public class Response<T> {
 
-    public String error;
-    public T data;
+    private String error;
+    private T data;
 
-    public Response(String error, T data) {
+    private Response(String error, T data) {
         this.error = error;
         this.data = data;
     }
 
+    @SuppressWarnings("unchecked")
     public static <T> Response<T> ok(T data){
         return new Response(null,data);
     }
 
+    @SuppressWarnings("unchecked")
     public static <T> Response<T> error(String message){
         return new Response(message,null);
     }

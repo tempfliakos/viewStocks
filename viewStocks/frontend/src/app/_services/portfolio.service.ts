efbb4c9b6194101f2b349ec6d38b2a5fb18737fc
Bence Knáb <benceknab.iac@gmail.com>
2017-12-11 17:01:10 +0100

    quick fixes

--- viewStocks/frontend/src/app/_services/portfolio.service.ts
+++ viewStocks/frontend/src/app/_services/portfolio.service.ts
@@ -1,30 +1,31 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { URL } from '../_constants';
-import { Portfolio } from '../_models/portfolio';
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {URL} from '../_constants';
+import {Portfolio} from '../_models';
 
 @Injectable()
 export class PortfolioService {
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+  }
 
   add(portfolio: Portfolio) {
     return this.http.post(`${URL}/portfolio/add`, portfolio);
   }
 
   delete(portfolio: Portfolio) {
-    return this.http.post(`${URL}/portfolio/delete`, portfolio);
+    // return this.http.delete(`${URL}/portfolio/delete`, portfolio);
   }
 
   update(portfolio: Portfolio) {
-    return this.http.post(`${URL}/portfolio/update`, portfolio);
+    return this.http.put(`${URL}/portfolio/update`, portfolio);
   }
 
-  findById(id: number) {
-    return this.http.post(`${URL}/portfolio/get`, id);
+  findById(portfolio: Portfolio) {
+    return this.http.get(`${URL}/portfolio/get?portfolioId=${portfolio.id}`);
   }
 
-  getOwnedPortfolios(id: number) {
-    return this.http.post(`${URL}/portfolio/getOwnedPortfolios`, id);
+  getOwnedPortfolios(portfolio: Portfolio) {
+    return this.http.get(`${URL}/portfolio/getOwnedPortfolios?userId=${portfolio.owner.id}`);
   }
 
 }
